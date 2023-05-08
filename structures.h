4563b4417b747117b38f4894aa398568fcd86a05
21DP1RRoze <103510238+21DP1RRoze@users.noreply.github.com>
2023-05-08 13:35:55 +0000

    structures

--- structures.h
+++ structures.h
@@ -4,3 +4,15 @@ struct {
     std::string name;
     std::string workerSince;
 } Worker;
+
+struct {
+    std::string name;
+    std::string dateSince;
+    float value;
+} Item;
+
+struct {
+    int sectionX;
+    int sectionY;
+    int sectionZ;
+} Shelf;
