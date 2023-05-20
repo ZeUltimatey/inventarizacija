07d5e0e354d57bdade5bbcb70db7bcd720b1cb81
Edijs Aizgirevičs <99726778+ZeUltimatey@users.noreply.github.com>
2023-05-20 18:35:48 +0300

    adding stuff

---
+++ cmake-build-debug/CMakeFiles/inventarizacija.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inventarizacija.dir/main.cpp.obj"
+  "CMakeFiles/inventarizacija.dir/main.cpp.obj.d"
+  "inventarizacija.exe"
+  "inventarizacija.exe.manifest"
+  "inventarizacija.pdb"
+  "libinventarizacija.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inventarizacija.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
